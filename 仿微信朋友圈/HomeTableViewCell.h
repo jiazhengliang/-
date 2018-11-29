a50b6f1bbf237204658524e311ba77f59f952f03
jonejia <jone@JonedeMac-mini.local>
2018-11-29 10:46:07 +0800

    first commit

---
+++ 仿微信朋友圈/HomeTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  HomeTableViewCell.h
+//  仿微信朋友圈
+//
+//  Created by 苗建浩 on 2017/1/5.
+//  Copyright © 2017年 苗建浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeTableViewCell : UITableViewCell
+
+@end
---
+++ 仿微信朋友圈/HomeTableViewCell.m
@@ -0,0 +1,24 @@
+//
+//  HomeTableViewCell.m
+//  仿微信朋友圈
+//
+//  Created by 苗建浩 on 2017/1/5.
+//  Copyright © 2017年 苗建浩. All rights reserved.
+//
+
+#import "HomeTableViewCell.h"
+
+@implementation HomeTableViewCell
+
+- (void)awakeFromNib {
+    [super awakeFromNib];
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
